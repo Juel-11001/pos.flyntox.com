58f303325d0e3412cb2944801128df5dcf76f29c
Inoodex Hasan <hasan@inoodex.com>
2026-04-23 18:35:47 +0600

    browse text fixed

--- resources/views/components/document_help_text.blade.php
+++ resources/views/components/document_help_text.blade.php
@@ -1,9 +1,2 @@
 <br>
-@lang('lang_v1.allowed_file'):
-	@foreach(config('constants.document_upload_mimes_types') as $key => $value)
-		@if(!$loop->last)
-			{{$value . ','}}
-		@else
-			{{$value}}
-	    @endif
-	@endforeach
+@lang('lang_v1.allowed_file'): {{ implode(', ', array_values(config('constants.document_upload_mimes_types'))) }}
